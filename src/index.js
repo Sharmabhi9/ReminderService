24ac335b8a322f56230d79939d535850180be77c
abhishek sharma <abhisheksharmaqwr0123@gmail.com>
2024-08-17 23:27:48 +0530

    Basic Setup Of Server

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const {PORT}=require('./config/serverConfig');
+
+const setupAndstartserver = () =>{
+    const app=express();
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.listen(PORT,()=>{
+        console.log(`Sever running at ${PORT}`);
+        console.log(bodyParser);
+    })
+
+}
+
+setupAndstartserver();
